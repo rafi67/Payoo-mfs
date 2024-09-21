5d8fca2bc4868b2885a6732ed10ce18e61b54d21
Rafi Siddique <rafisiddique67@gmail.com>
2024-09-21 23:16:22 +0600

    added partially the add money event handler

--- home.js
+++ home.js
@@ -1,1 +1,15 @@
-console.log('home added');
+
+// add money to the account
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('btn-add-money').addEventListener('click', function(event){
+        event.preventDefault();
+        
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        console.log(addMoneyInput);
+
+        //  get the pin number provided
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+        console.log(pinNumberInput);
+    });
+});
