545b73c6f4d1e6de40015bcd02e714c2136f755d
Rafi Siddique <rafisiddique67@gmail.com>
2024-09-22 00:47:53 +0600

    cash out feature added

---
+++ cashOut.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('cash-out').addEventListener('click', function(event){
+      event.preventDefault();
+      
+      const addMoney = document.getElementById('input-cash-out-money').value;
+      const addMoneyNumber = parseFloat(addMoney);
+      const pinNumberInput = document.getElementById('pin-number').value;
+      console.log(pinNumberInput)
+      if(pinNumberInput==='1234') {
+        const balance = document.getElementById('available-money').innerText;
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber - addMoneyNumber;
+        console.log('new balance =', newBalance);
+        document.getElementById('available-money').innerText = newBalance;
+      }
+      else {
+        alert('Failed to add money. Please try again later');
+      }
+    });
+});
