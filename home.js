984a3075942755813303228e63460ec7413ec820
Rafi Siddique <rafisiddique67@gmail.com>
2024-09-22 00:17:27 +0600

    balacne update handler added

--- home.js
+++ home.js
@@ -5,11 +5,20 @@ document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btn-add-money').addEventListener('click', function(event){
         event.preventDefault();
         
-        const addMoneyInput = document.getElementById('input-add-money').value;
+        const addMoneyInput = parseFloat(document.getElementById('input-add-money').innerText);
         console.log(addMoneyInput);
 
         //  get the pin number provided
-        const pinNumberInput = document.getElementById('input-pin-number').value;
+        const pinNumberInput = parseInt(document.getElementById('input-pin-number').value);
         console.log(pinNumberInput);
+
+        const availableMoney = document.getElementById('available-money').innerText;
+        var balanceNumber = parseFloat(availableMoney);
+
+        if(pinNumberInput=='1234') {
+           const newBalance = availableMoney + balanceNumber;
+           document.getElementById('available-money').innerText = newBalance;
+           console.log("new balance = ", newBalance);
+        }
     });
 });
